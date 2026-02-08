481920c47d7a1d41232ae77b08a16cb42f1b5602
Dane "Dev <81720436+developer9998@users.noreply.github.com>
2026-02-08 09:06:40 -0330

    minor changes

--- GorillaInfoWatch/Constants.cs
+++ GorillaInfoWatch/Constants.cs
@@ -3,7 +3,7 @@
     public class Constants
     {
         public const string GUID = "dev.gorillainfowatch";
-        public const string Name = "GorillaInfoWatch";
+        public const string Name = "GorillaInfoWatch Early Access";
         public const string Version = "1.1.2";
 
         public const int SectionCapacity = 14;
@@ -19,6 +19,6 @@
         public const string DataEntry_ShortcutName = "ShortcutName";
         public const string DataEntry_Consent = "Consent";
 
-        public const string Uri_LatestVersion = "https://raw.githubusercontent.com/developer9998/GorillaInfoWatch-Lite/main/LatestVersion.txt";
+        public const string Uri_LatestVersion = "https://raw.githubusercontent.com/developer9998/GorillaInfoWatch/main/LatestVersion.txt";
     }
 }
