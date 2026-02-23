88a59313d822eb95ed39e133a51f1344c3895738
Dane "Dev <81720436+developer9998@users.noreply.github.com>
2026-02-23 01:04:16 -0330

    Update ObjectExtensions.cs

--- GorillaInfoWatch/Extensions/ObjectExtensions.cs
+++ GorillaInfoWatch/Extensions/ObjectExtensions.cs
@@ -7,15 +7,9 @@ internal static class ObjectExtensions
     // https://github.com/developer9998/Bark/blob/master/Extensions/GameObjectExtensions.cs
     // "Comfort in community, obliterated"
 
-    public static void Obliterate(this GameObject gameObject)
-    {
-        Object.Destroy(gameObject);
-    }
+    public static void Obliterate(this GameObject gameObject) => Object.Destroy(gameObject);
 
-    public static void Obliterate(this Component component)
-    {
-        Object.Destroy(component);
-    }
+    public static void Obliterate(this Component component) => Object.Destroy(component);
 
     public static bool Exists(this Object obj) => obj != null && obj;
 
